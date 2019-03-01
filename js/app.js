b0d04500f146b41d2f2576c47277902cac4c368a
sk-agnihotri <sk-agnihotri@users.noreply.github.com>
2019-03-01 02:13:34 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,116 +1,136 @@
-# Keep the current chart so we can manip it later
-myChart = null
+var DATASET_COLORS, addColorToDataset, errorWrapped, getCurrentViz, getCurrentWorksheet, getTableau, initChart, myChart, updateChartWithData,
+  slice = [].slice;
 
-# Quick accessors for accessing the tableau bits on the parent page
-getTableau = ()-> parent.parent.tableau
-getCurrentViz = ()-> getTableau().VizManager.getVizs()[0]
-# Returns the current worksheet.
-# The path to access the sheet is hardcoded for now.
-getCurrentWorksheet = ()-> getCurrentViz().getWorkbook().getActiveSheet().getWorksheets()[0]
+myChart = null;
 
-# Because handlers in promises swallow errors and
-# the error callbacks for Promises/A are flaky,
-# we simply use this function to wrap calls
-errorWrapped = (context, fn)->
-  (args...)->
-    try
-      fn(args...)
-    catch err
-      console.error "Got error during '", context, "' : ", err
+getTableau = function() {
+  return parent.parent.tableau;
+};
 
-DATASET_COLORS =
-  "Furniture": "#f28e2b"
-  Technology: "#4e79a7"
+getCurrentViz = function() {
+  return getTableau().VizManager.getVizs()[0];
+};
+
+getCurrentWorksheet = function() {
+  return getCurrentViz().getWorkbook().getActiveSheet().getWorksheets()[0];
+};
+
+errorWrapped = function(context, fn) {
+  return function() {
+    var args, err, error;
+    args = 1 <= arguments.length ? slice.call(arguments, 0) : [];
+    try {
+      return fn.apply(null, args);
+    } catch (error) {
+      err = error;
+      return console.error("Got error during '", context, "' : ", err);
+    }
+  };
+};
+
+DATASET_COLORS = {
+  "Furniture": "#f28e2b",
+  Technology: "#4e79a7",
   "Office Supplies": "#e15759"
+};
 
+addColorToDataset = function(d, color) {
+  d.backgroundColor = color;
+  return d.hoverBackgroundColor = color;
+};
 
+updateChartWithData = function(datasets) {
+  var d, i, j, k, len, len1;
+  for (j = 0, len = datasets.length; j < len; j++) {
+    d = datasets[j];
+    addColorToDataset(d, DATASET_COLORS[d.data[0].category]);
+  }
+  if (myChart) {
+    for (i = k = 0, len1 = datasets.length; k < len1; i = ++k) {
+      d = datasets[i];
+      _.extend(myChart.data.datasets[i], d);
+    }
+    return myChart.update();
+  } else {
+    return myChart = new Chart(document.getElementById("chart"), {
+      type: "bubble",
+      data: {
+        datasets: datasets,
+        xLabels: ["Sales"],
+        yLabels: ["Profit"]
+      },
+      options: {
+        animation: {
+          duration: 1000
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [
+            {
+              scaleLabel: {
+                display: true,
+                labelString: "Profit"
+              }
+            }
+          ],
+          xAxes: [
+            {
+              scaleLabel: {
+                display: true,
+                labelString: "Sales"
+              }
+            }
+          ]
+        }
+      }
+    });
+  }
+};
 
-# set both colors for the dataset
-addColorToDataset = (d, color)->
-  d.backgroundColor = color
-  d.hoverBackgroundColor = color
-
-# Helper fn that creates a new chart with the required data
-updateChartWithData = (datasets)->
-
-  # At this point we should only have datasets that are not empty
-  addColorToDataset(d, DATASET_COLORS[d.data[0].category]) for d in datasets
-
-  if myChart
-    # Update the chart data sets by only updating the data.
-    # This way we'll have transition animations
-    for d,i in datasets
-      _.extend( myChart.data.datasets[i], d )
-    myChart.update()
-
-  else
-    myChart = new Chart document.getElementById("chart"),
-        type: "bubble"
-        data:
-          datasets: datasets
-          xLabels: ["Sales"]
-          yLabels: ["Profit"]
-        options:
-          animation: { duration: 1000}
-          responsive: true
-          maintainAspectRatio: false
-          scales:
-            yAxes: [{
-              scaleLabel: { display: true, labelString: "Profit" }
-            }]
-            xAxes: [{
-              scaleLabel: { display: true, labelString: "Sales" }
-            }]
-
-
-initChart = ->
-  # Get the tableau bits from the parent.
-  tableau = getTableau()
-
-  # Error handler in case getting the data fails in the Promise
-  onDataLoadError = (err)->
-    console.err("Error during Tableau Async request:", err)
-
-  # Handler for loading and converting the tableau data to chart data
-  onDataLoadOk = errorWrapped "Getting data from Tableau", (table)->
-      # Create a column name -> idx map
-      colIdxMaps = {}
-      colIdxMaps[c.getFieldName()] = c.getIndex() for c in table.getColumns()
-
-      # Decompose the ids
-      {Category, Sales, Profit} = colIdxMaps
-
-      # converts a Tableau Table row into a JSChart data entry
-      toChartEntry = (d)->
-        x: parseFloat(d[Sales].value).toFixed(2)
-        y: parseFloat(d[Profit].value).toFixed(2)
-        category: d[Category].value
+initChart = function() {
+  var onDataLoadError, onDataLoadOk, tableau, updateChart;
+  tableau = getTableau();
+  onDataLoadError = function(err) {
+    return console.err("Error during Tableau Async request:", err);
+  };
+  onDataLoadOk = errorWrapped("Getting data from Tableau", function(table) {
+    var Category, Profit, Sales, c, colIdxMaps, graphDataByCategory, j, len, ref, toChartEntry;
+    colIdxMaps = {};
+    ref = table.getColumns();
+    for (j = 0, len = ref.length; j < len; j++) {
+      c = ref[j];
+      colIdxMaps[c.getFieldName()] = c.getIndex();
+    }
+    Category = colIdxMaps.Category, Sales = colIdxMaps.Sales, Profit = colIdxMaps.Profit;
+    toChartEntry = function(d) {
+      return {
+        x: parseFloat(d[Sales].value).toFixed(2),
+        y: parseFloat(d[Profit].value).toFixed(2),
+        category: d[Category].value,
         r: 5
+      };
+    };
+    graphDataByCategory = _.chain(table.getData()).map(toChartEntry).groupBy("category").map(function(data, label) {
+      return {
+        label: label,
+        data: data
+      };
+    }).value();
+    return errorWrapped("Updating the chart", updateChartWithData)(graphDataByCategory);
+  });
+  updateChart = function() {
+    return getCurrentWorksheet().getUnderlyingDataAsync({
+      maxRows: 0,
+      ignoreSelection: false,
+      includeAllColumns: true,
+      ignoreAliases: true
+    }).then(onDataLoadOk, onDataLoadError);
+  };
+  return getCurrentViz().addEventListener(tableau.TableauEventName.MARKS_SELECTION, updateChart);
+};
 
-      # group by the category
-      graphDataByCategory = _.chain(table.getData())
-        .map(toChartEntry)
-        .groupBy("category")
-        .map (data, label)-> {label: label, data: data}
-        .value()
-
-      # Call the updater function to fill the chart
-      errorWrapped("Updating the chart", updateChartWithData)(graphDataByCategory)
-
-  # Handler that gets the selected data from tableau and sends it to the chart
-  # display function
-  updateChart = ()->
-    getCurrentWorksheet()
-      .getUnderlyingDataAsync({maxRows: 0, ignoreSelection: false, includeAllColumns: true, ignoreAliases: true})
-      .then(onDataLoadOk, onDataLoadError )
-
-  # Add an event listener for marks change events that simply loads the
-  # selected data to the chart
-  getCurrentViz().addEventListener( tableau.TableauEventName.MARKS_SELECTION,  updateChart)
-
-
-
-@appApi = {
-  initChart,
-  myChart
-}
+this.appApi = {
+  initChart: initChart,
+  myChart: myChart
+};
