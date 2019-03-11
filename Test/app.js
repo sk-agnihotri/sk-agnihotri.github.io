5747d879b3d19564dfb651b2b0a20087d23c0876
sk-agnihotri <sk-agnihotri@users.noreply.github.com>
2019-03-11 07:08:51 -0500

    Update app.js

--- Test/app.js
+++ Test/app.js
@@ -53,7 +53,7 @@ updateChartWithData = function(datasets) {
     return myChart.update();
   } else {
     return myChart = new Chart(document.getElementById("chart"), {
-      type: "bar",
+      type: "bubble",
       data: {
         datasets: datasets,
         xLabels: ["CYMTD_Detractor"],
